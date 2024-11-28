3c043ededd153eaa159fb431d2ff831666639782
joy-mahmud <joymahmudiucse@gmail.com>
2024-11-28 12:47:36 +0600

    dynamic route added

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    name='habib'
+    id=101
+    return render_template('home.html',name=name,id=id)
+
+@app.route("/user")
+@app.route('/user/<name>')
+def user(name="joy"):
+    return f"hello {name}"
+
+@app.route('/post/<int:postid>/<commentid>')
+def post(postid,commentid):   
+    return f"post:{postid} comment:{commentid}"
 
 if __name__=="__main__":
     app.run(debug=True)
